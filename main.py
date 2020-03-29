093f3c3e96a9af3c3c5211b62cb92f7b120fb4d0
ddghost <544011312@qq.com>
2020-03-30 00:25:14 +0800

    update

--- main.py
+++ main.py
@@ -108,6 +108,7 @@ def main(datasetPath):
         test(val_loader, model, attr_num, description)
         return
     accu = validate(val_loader, model, criterion, 0)
+    test(val_loader, model, attr_num, description)
     print(accu,'!!!!!')
     for epoch in range(args.start_epoch, args.epochs):
         adjust_learning_rate(optimizer, epoch, args.decay_epoch)
@@ -250,55 +251,55 @@ def test(val_loader, model, attr_num, description):
         pos_tol.append(0)
         neg_cnt.append(0)
         neg_tol.append(0)
+    with torch.no_grad():
+        for i, _ in enumerate(val_loader):
+            input, target = _
+            target = target.cuda(non_blocking=True)
+            input = input.cuda(non_blocking=True)
+            output = model(input)
+            bs = target.size(0)
 
-    for i, _ in enumerate(val_loader):
-        input, target = _
-        target = target.cuda(non_blocking=True)
-        input = input.cuda(non_blocking=True)
-        output = model(input)
-        bs = target.size(0)
-
-        # maximum voting
-        if type(output) == type(()) or type(output) == type([]):
-            output = torch.max(torch.max(torch.max(output[0],output[1]),output[2]),output[3])
+            # maximum voting
+            if type(output) == type(()) or type(output) == type([]):
+                output = torch.max(torch.max(torch.max(output[0],output[1]),output[2]),output[3])
 
 
-        batch_size = target.size(0)
-        tol = tol + batch_size
-        output = torch.sigmoid(output.data).cpu().numpy()
-        output = np.where(output > 0.5, 1, 0)
-        target = target.cpu().numpy()
+            batch_size = target.size(0)
+            tol = tol + batch_size
+            output = torch.sigmoid(output.data).cpu().numpy()
+            output = np.where(output > 0.5, 1, 0)
+            target = target.cpu().numpy()
 
-        for it in range(attr_num):
-            for jt in range(batch_size):
-                if target[jt][it] == 1:
-                    pos_tol[it] = pos_tol[it] + 1
-                    if output[jt][it] == 1:
-                        pos_cnt[it] = pos_cnt[it] + 1
-                if target[jt][it] == 0:
-                    neg_tol[it] = neg_tol[it] + 1
-                    if output[jt][it] == 0:
-                        neg_cnt[it] = neg_cnt[it] + 1
-
-        if attr_num == 1:
-            continue
-        for jt in range(batch_size):
-            tp = 0
-            fn = 0
-            fp = 0
             for it in range(attr_num):
-                if output[jt][it] == 1 and target[jt][it] == 1:
-                    tp = tp + 1
-                elif output[jt][it] == 0 and target[jt][it] == 1:
-                    fn = fn + 1
-                elif output[jt][it] == 1 and target[jt][it] == 0:
-                    fp = fp + 1
-            if tp + fn + fp != 0:
-                accu = accu +  1.0 * tp / (tp + fn + fp)
-            if tp + fp != 0:
-                prec = prec + 1.0 * tp / (tp + fp)
-            if tp + fn != 0:
-                recall = recall + 1.0 * tp / (tp + fn)
+                for jt in range(batch_size):
+                    if target[jt][it] == 1:
+                        pos_tol[it] = pos_tol[it] + 1
+                        if output[jt][it] == 1:
+                            pos_cnt[it] = pos_cnt[it] + 1
+                    if target[jt][it] == 0:
+                        neg_tol[it] = neg_tol[it] + 1
+                        if output[jt][it] == 0:
+                            neg_cnt[it] = neg_cnt[it] + 1
+
+            if attr_num == 1:
+                continue
+            for jt in range(batch_size):
+                tp = 0
+                fn = 0
+                fp = 0
+                for it in range(attr_num):
+                    if output[jt][it] == 1 and target[jt][it] == 1:
+                        tp = tp + 1
+                    elif output[jt][it] == 0 and target[jt][it] == 1:
+                        fn = fn + 1
+                    elif output[jt][it] == 1 and target[jt][it] == 0:
+                        fp = fp + 1
+                if tp + fn + fp != 0:
+                    accu = accu +  1.0 * tp / (tp + fn + fp)
+                if tp + fp != 0:
+                    prec = prec + 1.0 * tp / (tp + fp)
+                if tp + fn != 0:
+                    recall = recall + 1.0 * tp / (tp + fn)
 
     print('=' * 100)
     print('\t     Attr              \tp_true/n_true\tp_tol/n_tol\tp_pred/n_pred\tcur_mA')
