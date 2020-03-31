e87d75ef975de51c289e3edbb079f7a16c20ebd6
ddghost <544011312@qq.com>
2020-03-31 14:40:30 +0800

    update

--- model/deepMAR.py
+++ model/deepMAR.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn.init as init
-import .resnet as resnet
+from .resnet import resnet
 
 __all__ = ['DeepMAR_ResNet50']
 
